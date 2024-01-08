0a1043880c92544c4be4c1e0e5c02b6335f61ee3
Mozart Rueckert <mozartrue@outlook.com>
2024-01-07 22:32:56 -0300

    feat: add user routes

--- src/http/controllers/user/routes.ts
+++ src/http/controllers/user/routes.ts
@@ -1,6 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./register";
+import { login } from "./login";
+import { refresh } from "./refresh";
 
 export async function userRoutes(app: FastifyInstance) {
   app.post("/user/register", register);
+  app.post("/user/login", login);
+  app.patch("/token/refresh", refresh);
 }
