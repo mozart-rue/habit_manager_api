a52668d14bdcd1490a9ef1ca9359d6eb6d3f1797
Mozart Rueckert <mozartrue@outlook.com>
2024-01-07 22:34:03 -0300

    feat: register cookies and fastifyJwt

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
 import fastify from "fastify";
+import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
 import { ZodError } from "zod";
 import { env } from "./env";
 import { userRoutes } from "./http/controllers/user/routes";
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
+});
+
+app.register(fastifyCookie);
 
 app.register(userRoutes);
 
