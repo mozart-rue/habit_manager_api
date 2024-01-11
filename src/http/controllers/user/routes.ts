47eba24a202e77ab893a5a538279200443f65f64
Mozart Rueckert <mozartrue@outlook.com>
2024-01-10 23:08:25 -0300

    feat: set authenticated route top habits

--- src/http/controllers/user/routes.ts
+++ src/http/controllers/user/routes.ts
@@ -2,9 +2,18 @@ import { FastifyInstance } from "fastify";
 import { register } from "./register";
 import { login } from "./login";
 import { refresh } from "./refresh";
+import { verifyJWT } from "../../middleware/verify-jwt";
+import { topHabitsAndGoals } from "./top-habits-and-goals";
 
 export async function userRoutes(app: FastifyInstance) {
   app.post("/user/register", register);
   app.post("/user/login", login);
   app.patch("/token/refresh", refresh);
+
+  // Authenticate Routes
+  app.get(
+    "/user/top_habits_and_goals",
+    { onRequest: [verifyJWT] },
+    topHabitsAndGoals,
+  );
 }
