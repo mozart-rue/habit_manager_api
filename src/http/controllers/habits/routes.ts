fe539b3f5e3a7de975ab6ff458327f9a99460970
Mozart Rueckert <mozartrue@outlook.com>
2024-01-12 19:51:34 -0300

    feat: set route to remove habit

--- src/http/controllers/habits/routes.ts
+++ src/http/controllers/habits/routes.ts
@@ -3,6 +3,7 @@ import { verifyJWT } from "../../middleware/verify-jwt";
 import { create } from "./create";
 import { fetchAll } from "./fetch-all";
 import { edit } from "./edit";
+import { remove } from "./remove";
 
 export async function habitRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
@@ -10,4 +11,5 @@ export async function habitRoutes(app: FastifyInstance) {
   app.post("/habits/create", create);
   app.get("/habits/fetchAll", fetchAll);
   app.put("/habits/edit/:habitId", edit);
+  app.delete("/habits/:habitId", remove);
 }
