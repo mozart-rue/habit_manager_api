dad46b541f4783ddb5bdad62b1eab21915a20f9c
Mozart Rueckert <mozartrue@outlook.com>
2024-01-12 19:32:55 -0300

    feat: set route to edit an habit

--- src/http/controllers/habits/routes.ts
+++ src/http/controllers/habits/routes.ts
@@ -2,10 +2,12 @@ import { FastifyInstance } from "fastify";
 import { verifyJWT } from "../../middleware/verify-jwt";
 import { create } from "./create";
 import { fetchAll } from "./fetch-all";
+import { edit } from "./edit";
 
 export async function habitRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
 
   app.post("/habits/create", create);
   app.get("/habits/fetchAll", fetchAll);
+  app.put("/habits/edit/:habitId", edit);
 }
