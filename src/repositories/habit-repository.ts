edd14105f9a265d9222fcd10f84991d5caa1e3bd
Mozart Rueckert <mozartrue@outlook.com>
2024-01-11 23:42:55 -0300

    feat: add method fetch all

--- src/repositories/habit-repository.ts
+++ src/repositories/habit-repository.ts
@@ -2,5 +2,6 @@ import { Habit } from "@prisma/client";
 
 export interface HabitRepository {
   save(data: CreateHabit): Promise<Habit>;
+  fetchAll(userId: string): Promise<HabitHistory[]>;
   fetchTopThreeDailyHabits(userId: string): Promise<HabitHistory[]>;
 }
