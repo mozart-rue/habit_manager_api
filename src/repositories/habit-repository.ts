cc63f8673ed59f0a8fb7a1c09158dfb0381997fa
Mozart Rueckert <mozartrue@outlook.com>
2024-01-12 18:33:33 -0300

    feat: set method find by id

--- src/repositories/habit-repository.ts
+++ src/repositories/habit-repository.ts
@@ -4,4 +4,5 @@ export interface HabitRepository {
   save(data: CreateHabit): Promise<Habit>;
   fetchAll(userId: string): Promise<HabitHistory[]>;
   fetchTopThreeDailyHabits(userId: string): Promise<HabitHistory[]>;
+  findById(userId: string, habitId: string): Promise<Habit | null>;
 }
