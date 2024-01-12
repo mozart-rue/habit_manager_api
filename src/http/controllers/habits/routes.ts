c00c79f8249a5e0c99f3af0e1a48068cefff59f6
Mozart Rueckert <mozartrue@outlook.com>
2024-01-11 23:45:34 -0300

    feat: set route to fetch all habits controller

--- src/http/controllers/habits/routes.ts
+++ src/http/controllers/habits/routes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { verifyJWT } from "../../middleware/verify-jwt";
 import { create } from "./create";
+import { fetchAll } from "./fetch-all";
 
 export async function habitRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
 
   app.post("/habits/create", create);
+  app.get("/habits/fetchAll", fetchAll);
 }
