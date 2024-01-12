fdc8fb1530298f37b4ead7511451a61d4ea77cf6
Mozart Rueckert <mozartrue@outlook.com>
2024-01-11 22:43:23 -0300

    feat: add method save

--- src/repositories/habit-repository.ts
+++ src/repositories/habit-repository.ts
@@ -1,3 +1,6 @@
+import { Habit } from "@prisma/client";
+
 export interface HabitRepository {
+  save(data: CreateHabit): Promise<Habit>;
   fetchTopThreeDailyHabits(userId: string): Promise<HabitHistory[]>;
 }
