558dd398351f32045cc929b643eec1c6abc91493
Mozart Rueckert <mozartrue@outlook.com>
2024-01-12 19:28:59 -0300

    feat: set method edit

--- src/repositories/habit-repository.ts
+++ src/repositories/habit-repository.ts
@@ -1,8 +1,25 @@
 import { Habit } from "@prisma/client";
 
+export interface UpdateHabit {
+  userId: string;
+  habitId: string;
+  goal_name: string;
+  habit_name: string;
+  frequency: string;
+  period_end: Date;
+}
+
 export interface HabitRepository {
   save(data: CreateHabit): Promise<Habit>;
   fetchAll(userId: string): Promise<HabitHistory[]>;
   fetchTopThreeDailyHabits(userId: string): Promise<HabitHistory[]>;
   findById(userId: string, habitId: string): Promise<Habit | null>;
+  edit({
+    userId,
+    habitId,
+    goal_name,
+    habit_name,
+    frequency,
+    period_end,
+  }: UpdateHabit): Promise<Habit>;
 }
